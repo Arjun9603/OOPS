3da0e42f077bbc14dd8b74bca2d1bd604b4df8e2
Arjun9603 <arjunraghav774@gmail.com>
2025-06-30 23:57:10 +0530

    Types of Inheritance programs

--- Inheritance/Inherit2.java
+++ Inheritance/Inherit2.java
@@ -17,17 +17,30 @@ class D extends B{
     void showC(){
         System.out.println("C class Method");
     }
+}
+
+class E extends A{
+
 
     public static void main(String[] args) {
         A obj=new A();
         obj.showA(); //Only A class method will be called by object of class A
+        System.out.println("-----------------------");
         B obj1=new B();
         obj1.showA(); // Method of both child and parent class is called by object of child/subclass object.
         obj1.showB();
+        System.out.println("-----------------------");
 
         D obj2=new D();
         obj2.showA();
         obj2.showB();
         obj2.showC(); // MultiLevel Inheritance.
-    }
+        System.out.println("----------------------");
+
+        E obj5=new E(); // obj5 only access A class methods.
+        obj5.showA();
+        
+    } //Multiple Inheritance is not available in JAVA due to ambiguity error.
+    //Hybrid Inheritance i also absent due to the combination of Multiple and Hierarchial Inheritance.
 }
+
